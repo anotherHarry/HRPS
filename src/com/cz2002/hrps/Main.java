ead5cb7588e5eba6670014639c46b076b2325130
harry <ngocthaicao@gmail.com>
2018-04-13 18:30:13 +0800

    add main

---
+++ src/com/cz2002/hrps/Main.java
@@ -0,0 +1,16 @@
+package com.cz2002.hrps;
+
+/**
+ * Main Class
+ */
+public class Main {
+
+  /**
+   * main is the entrance to start the app
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+
+}
